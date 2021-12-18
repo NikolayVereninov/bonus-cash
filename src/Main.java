f7040710cb153e8ae24c3b4409e12640f898c39d
NikolayVereninov <89935534+NikolayVereninov@users.noreply.github.com>
2021-12-18 23:44:24 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         boolean registered = true;
-        int balance = 100;
-        int addBalance = 2000;
-        int percentBonus = 20;
-        int amount = addBalance * percentBonus;
-
-        System.out.println(percentBonus);
+        int ticketprice = 1000;
+        int rubles = 20;
+        int bonusMiles = ticketprice / rubles;
+        System.out.println(bonusMiles);
     }
 }
