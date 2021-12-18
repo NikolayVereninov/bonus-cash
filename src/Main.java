7479efe089bd5f189ab2a792d5b7ef24eb00cc97
NikolayVereninov <kolia19741974@gmail.com>
2021-12-18 12:48:13 +0300

    add file

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean registered = true;
+        int balance = 100;
+        int addBalance = 2000;
+        int percentBonus = 20;
+        int amount = addBalance * percentBonus;
+
+        System.out.println(percentBonus);
+    }
+}
