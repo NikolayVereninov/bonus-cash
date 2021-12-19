e5b9b59c10c6d7807b0afd9eb1ee98fcee1be9a1
NikolayVereninov <89935534+NikolayVereninov@users.noreply.github.com>
2021-12-19 13:56:44 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         boolean registered = true;
-        int ticketprice = 1000;
+        int ticketPrice = 1000;
         int rubles = 20;
-        int bonusMiles = ticketprice / rubles;
+        int bonusMiles = ticketPrice / rubles;
         System.out.println(bonusMiles);
     }
 }
